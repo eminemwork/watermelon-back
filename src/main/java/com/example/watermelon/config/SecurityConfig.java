6609cb4f2b41be9d4ffd469e210c10b579fdf709
EasyZero <eminem54@naver.com>
2020-06-18 22:55:31 +0900

    Na: na

---
+++ src/main/java/com/example/watermelon/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.watermelon.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .formLogin()
+                .disable()
+                .httpBasic()
+                .disable()
+                .exceptionHandling()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll()
+                .antMatchers("/auth/**", "/oauth2/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login();
+    }
+}
